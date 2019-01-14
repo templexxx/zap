6ccc613e8b901c3de10f19020453f778aa742084
templexxx <lucas1x1x@gmail.com>
2019-01-14 16:00:55 +0800

    add sync & fadivse clean cache

--- zapcore/writebuf_test.go
+++ zapcore/writebuf_test.go
@@ -71,4 +71,29 @@ func TestIoCore_ReOpen(t *testing.T) {
 		string(logged),
 		"Unexpected log output.",
 	)
+
+	np2 := "zapcore-drop2"
+	os.Rename(temp.Name(), np2)
+	defer os.Remove(np2)
+	core.ReOpen()
+
+	if ce := core.Check(Entry{Level: DebugLevel, Message: "debug"}, nil); ce != nil {
+		ce.Write(makeInt64Field("k", 2))
+	}
+	if ce := core.Check(Entry{Level: InfoLevel, Message: "info"}, nil); ce != nil {
+		ce.Write(makeInt64Field("k", 3))
+	}
+	if ce := core.Check(Entry{Level: WarnLevel, Message: "warn"}, nil); ce != nil {
+		ce.Write(makeInt64Field("k", 4))
+	}
+	core.Sync()
+	logged, err = ioutil.ReadFile(temp.Name())
+	require.NoError(t, err, "Failed to read from temp file.")
+	require.Equal(
+		t,
+		`{"level":"info","msg":"info","k":1,"k":3}`+"\n"+
+			`{"level":"warn","msg":"warn","k":1,"k":4}`+"\n",
+		string(logged),
+		"Unexpected log output.",
+	)
 }
